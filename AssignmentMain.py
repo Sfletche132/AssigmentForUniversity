9338780021105acb7e3dcb927455b0da8cb52ff7
Sfletche132 <36818350+Sfletche132@users.noreply.github.com>
2018-03-02 13:00:01 +0000

    added interp function for e field

--- AssignmentMain.py
+++ AssignmentMain.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math as math
+
 
 
 def open_file(filename):  # Opens file and reads it, returning a list of the lines and closes the file
@@ -23,13 +25,13 @@ def get_lines(inputlist):  # opens the list of lines, splits spaces, adds them t
 
 EX_datalist = get_lines(EX_dataraw[1:])
 EY_datalist = get_lines(EY_dataraw[1:])
-print(len(EX_datalist[0]))
+
 
 
 def find_e_strength(Ex_list, Ey_list):  # inputs two lists of lines and outputs the resulting e field strength
     matrix_ex = np.nan_to_num(np.array(Ex_list, dtype=float))  # dealing with nan values and converting to numpy array
     matrix_ey = np.nan_to_num(np.array(Ey_list, dtype=float))
-    field_strength = np.sqrt(((matrix_ex ** 2) + (matrix_ey ** 2)))
+    field_strength = np.sqrt(np.square(matrix_ex) + np.square(matrix_ey))
     return field_strength
 
 
@@ -46,3 +48,25 @@ def find_e_angle(Ex_list, Ey_list):
 e_field_strength = find_e_strength(EX_datalist, EY_datalist)
 e_field_angle = find_e_angle(EX_datalist, EY_datalist)
 
+
+def interp_efield(desiredx, inputlist):  # type_ float , list -> list
+    percent_of_file = desiredx / 5.0
+    desired_index = round(101 * percent_of_file, 4)
+    rounded_index = int(math.floor(desired_index))
+    result = []
+    for line in inputlist:
+        percentage = (line[rounded_index + 1] - line[rounded_index]) * abs(desired_index - rounded_index)
+        result.append(float(percentage + line[rounded_index]))
+    return result
+
+
+efieldyvalues = interp_efield(0.5, EY_datalist)
+
+print(efieldyvalues)
+# Don't really understand why have to limit between 0 - 5 metres
+# when the input data only exists within 0 - 5 metres ,
+# so a limit is not really needed
+# plt.xlim((0, 5))
+x_values = np.arange(0, 5, (5 / 101))
+plt.semilogx(x_values, efieldyvalues)
+plt.show()
