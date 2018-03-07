b67536720467fb3227ed6df64094cf6486b9aadf
Sfletche132 <36818350+Sfletche132@users.noreply.github.com>
2018-03-07 12:17:31 +0000

    Added comments Fixed the interpolation function

--- AssignmentMain.py
+++ AssignmentMain.py
@@ -1,6 +1,7 @@
+import math as math
+
 import matplotlib.pyplot as plt
 import numpy as np
-import math as math
 from matplotlib.ticker import AutoMinorLocator
 
 
@@ -16,31 +17,30 @@ EX_dataraw = open_file("Ex_grid.txt")
 EY_dataraw = open_file("Ey_grid.txt")
 
 
-def get_lines(inputlist):  # opens the list of lines, splits spaces, adds them to a list, outputs a list of line lists
+def get_lines(inputlist):  # opens the list of lines, splits spaces, adds values to a list, outputs a list of line lists
     lines_list = []
     for line in inputlist:
         lines_list.append([x for x in line.split()])
     return lines_list
 
 
+# calling the function which splits the lines and makes the data usable
 EX_datalist = get_lines(EX_dataraw[1:])
 EY_datalist = get_lines(EY_dataraw[1:])
 
 
-def find_e_strength(Ex_list, Ey_list):  # inputs two lists of lines and outputs the resulting e field strength
-    matrix_ex = np.array(Ex_list, dtype=float)  # converting to numpy array
+def find_e_strength(Ex_list, Ey_list):  # inputs two lists of lines and outputs e field strength as numpy array
+    matrix_ex = np.array(Ex_list, dtype=float)  # converting to numpy array to ease matrix multiplication
     matrix_ey = np.array(Ey_list, dtype=float)
     field_strength = np.sqrt(np.square(matrix_ex) + np.square(matrix_ey))
     return field_strength
 
 
-np.seterr(divide='ignore', invalid='ignore')  # To stop divide by zero errors
-
-
-def find_e_angle(Ex_list, Ey_list):
+def find_e_angle(Ex_list, Ey_list): # inputs two lists of lines and returns a numpy array of angles of e field
     matrix_ex = np.array(Ex_list, dtype=float)  # converting to numpy array
     matrix_ey = np.array(Ey_list, dtype=float)
     angle = np.arctan(np.divide(matrix_ey, matrix_ex, out=np.zeros_like(matrix_ey), where=matrix_ex != 0))
+    # divide calculation only divides when matrix_ex != 0 but when matrix_ex = 0 , 0 is outputted
     return angle
 
 
@@ -48,21 +48,19 @@ e_field_strength = find_e_strength(EX_datalist, EY_datalist)
 e_field_angle = find_e_angle(EX_datalist, EY_datalist)
 
 
-def interp_efield(desiredx, inputlist):  # type_ float , list -> list
-    percent_of_file = desiredx / 5.0
-    desired_index = round(101 * percent_of_file, 4)
+def interp_efield(desired_y, inputlist):
+    # inputs a desired y between 0 - 5 m , the inputted array is then interpolated so any y value can be found
+    percent_of_file = desired_y / 5.0
+    desired_index = 101 * percent_of_file
     rounded_index = int(math.floor(desired_index))
-    result = []
-    for line in inputlist:
-        percentage = (float(line[rounded_index + 1]) - float(line[rounded_index])) * abs(desired_index - rounded_index)
-        result.append(float(percentage) + float(line[rounded_index]))
-    return result
+    percentage = (inputlist[rounded_index + 1] - inputlist[rounded_index]) * abs(desired_index - rounded_index)
+    return percentage + inputlist[rounded_index]
 
 
 efieldyvalues = interp_efield(0.5, e_field_strength)
-x_values = np.arange(0, 5, (5 / 101))
+x_values = np.arange(0, 5, (5 / 101))  # X values are generated between 0 - 5 m to limit the plot range
 plt.plot(x_values, efieldyvalues, 'r-*', label='Electric field strength at y = 0.5m')
-plt.xscale('log')
+plt.xscale('log')  # setting x axis to logarithmic scale
 plt.xlabel("Distance (m)", fontsize=15)
 plt.ylabel("Electric field strength (V/m) ", fontsize=15)
 plt.legend(numpoints=1, loc='upper center', bbox_to_anchor=(0.32, 0.95), fancybox=True, shadow=True, ncol=5)
@@ -74,9 +72,10 @@ plt.tight_layout()
 plt.show()
 
 
-def save_file(filename,array,header,format):
-    np.savetxt(filename,array,fmt=format,header=header)
+def save_file(filename, array, header, format):  # This function saves the file using the numpy savetxt method
+    np.savetxt(filename, array, fmt=format, header=header)
 
 
-save_file("Deg_grid.txt", e_field_angle, "E field angle in uniform X Y grid", "%1.2f")
+# saving both files with headers and converting angles into degrees
+save_file("Deg_grid.txt", np.degrees(e_field_angle), "E field angle in uniform X Y grid", "%1.2f")
 save_file("Etot_grid.txt", e_field_strength, "E field strength in uniform X Y grid", "%1.5e")
