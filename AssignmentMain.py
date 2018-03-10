2b7caa1240512baa5900b9cc3a51566ce4ca273c
Sfletche132 <36818350+Sfletche132@users.noreply.github.com>
2018-03-10 14:33:24 +0000

    Added documentation

--- AssignmentMain.py
+++ AssignmentMain.py
@@ -1,5 +1,4 @@
 import math as math
-
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import AutoMinorLocator
@@ -7,6 +6,10 @@ from matplotlib.ticker import LogLocator
 
 
 def open_file(filename):  # Opens file and reads it, returning a list of the lines and closes the file
+    """
+    param filename = string for the file name
+    returns a list of lines, where a line is a string delimited by spaces
+    """
     f = open(filename, "r")
     input_data = f.readlines()
     f.close()
@@ -18,7 +21,11 @@ EX_dataraw = open_file("Ex_grid.txt")
 EY_dataraw = open_file("Ey_grid.txt")
 
 
-def get_lines(inputlist):  # opens the list of lines, splits spaces, adds values to a list, outputs a list of line lists
+def get_lines(inputlist):
+    """
+    param inputlist = list of raw data from the file, with each line as a string in the list
+    returns a nested list, with each value having an index within the nested list
+    """
     lines_list = []
     for line in inputlist:
         lines_list.append([x for x in line.split()])
@@ -30,27 +37,44 @@ EX_datalist = get_lines(EX_dataraw[1:])
 EY_datalist = get_lines(EY_dataraw[1:])
 
 
-def find_e_strength(Ex_list, Ey_list):  # inputs two lists of lines and outputs e field strength as numpy array
-    matrix_ex = np.array(Ex_list, dtype=float)  # converting to numpy array to ease matrix multiplication
+def find_e_strength(Ex_list, Ey_list):
+    """
+    param Ex_list = Equidistant grid of Ex values inputted as a list of lines
+    param Ey_list = Equidistant grid of Ey values inputted as a list of lines
+    returns a list of electric field strength values in an equidistant grid
+    """
+    matrix_ex = np.array(Ex_list, dtype=float)  # converting to numpy array
     matrix_ey = np.array(Ey_list, dtype=float)
-    field_strength = np.sqrt(np.square(matrix_ex) + np.square(matrix_ey))
+    #  Performing the electric field strength calculation
+    field_strength = np.sqrt(np.square(matrix_ex) + np.square(matrix_ey)) #
     return field_strength
 
 
-def find_e_angle(Ex_list, Ey_list): # inputs two lists of lines and returns a numpy array of angles of e field
+def find_e_angle(Ex_list, Ey_list):
+    """
+    param Ex_list = Equidistant grid of Ex values inputted as a list of lines
+    param Ey_list = Equidistant grid of Ey values inputted as a list of lines
+    returns list of angles in the electric field
+    """
     matrix_ex = np.array(Ex_list, dtype=float)  # converting to numpy array
     matrix_ey = np.array(Ey_list, dtype=float)
+    # performing angle calculation and handling divide by zero errors
     angle = np.arctan(np.divide(matrix_ey, matrix_ex, out=np.zeros_like(matrix_ey), where=matrix_ex != 0))
     # divide calculation only divides when matrix_ex != 0 but when matrix_ex = 0 , 0 is outputted
     return angle
 
 
+# calling the two function of electric field strength and angle for the different data lists
 e_field_strength = find_e_strength(EX_datalist, EY_datalist)
 e_field_angle = find_e_angle(EX_datalist, EY_datalist)
 
 
 def interp_efield(desired_y, inputlist):
-    # inputs a desired y between 0 - 5 m , the inputted list is then interpolated so any y value can be found
+    """
+    param desired_y = desired y between 0 - 5 m , on the grid of electric field strength values
+    param inputlist = The electric field strength list input
+    returns the row that the desired y corresponds to, which has been interpolated
+    """
     percent_of_file = desired_y / 5.0
     desired_index = 101 * percent_of_file
     rounded_index = int(math.floor(desired_index))
@@ -60,24 +84,33 @@ def interp_efield(desired_y, inputlist):
 
 efieldyvalues = interp_efield(0.5, e_field_strength)
 x_values = np.arange(0, 5, (5 / 101))  # X values are generated between 0 - 5 m to limit the plot range
-plt.figure()
-ax1 = plt.subplot(1, 1, 1)
+plt.figure()  # initialising figure
+ax1 = plt.subplot(1, 1, 1)  # Assigning figure subplot region
 ax1.plot(x_values, efieldyvalues, 'r-*', label='Electric field strength at y = 0.5m')
 ax1.set_xscale('log')  # setting x axis to logarithmic scale
 ax1.set_xlabel("Distance (m)", fontsize=15)
 ax1.set_ylabel("Electric field strength (V/m) ", fontsize=15)
 ax1.legend(numpoints=1, loc='upper center', bbox_to_anchor=(0.32, 0.95), fancybox=True, shadow=True, ncol=5)
+# Customising Ticks
 ax1.tick_params(axis="both", which="major", labelsize=15, color="b", width=1, length=5, labelcolor="black")
 ax1.yaxis.set_minor_locator(AutoMinorLocator(5))
 ax1.xaxis.set_minor_locator(LogLocator(10,subs="auto", numticks=10))
 ax1.yaxis.set_tick_params(which='minor', right='false', width=0.7, size=2)
 ax1.xaxis.set_tick_params(which='minor', right='false', width=0.7, size=2)
+# Showing plot
 ax1.grid()
 plt.tight_layout()
 plt.show()
 
 
-def save_file(filename, array, header, format):  # This function saves the file using the numpy savetxt method
+def save_file(filename, array, header, format):
+    """
+    param filename = name of the file you want to save
+    param array = data to be written into file
+    param header = header to be placed at the top of file
+    param format = Allows for the format of the data to be specified
+    returns nothing , file is saved locally with given data and header
+    """
     np.savetxt(filename, array, fmt=format, header=header)
 
 
